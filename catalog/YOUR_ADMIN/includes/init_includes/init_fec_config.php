50dd9bb62c42a4649db2ac859beb5576d6a0a490
numinix <webmaster@numinix.com>
2013-06-14 16:39:00 -0700

    v1.12.1
    - bug fix for min/max units in checkout

--- catalog/YOUR_ADMIN/includes/init_includes/init_fec_config.php
+++ catalog/YOUR_ADMIN/includes/init_includes/init_fec_config.php
@@ -4,7 +4,7 @@
   }
   // add upgrade script
   $fec_version = (defined('FAST_AND_EASY_CHECKOUT_VERSION') ? FAST_AND_EASY_CHECKOUT_VERSION : 'new');
-  $current_version = '1.12.0';
+  $current_version = '1.12.1';
   while ($fec_version != $current_version) {
     switch($fec_version) {
       case 'new':
@@ -20,7 +20,14 @@
           include_once(DIR_WS_INCLUDES . 'installers/fec/1_12_0.php');
           $fec_version = '1.12.0';          
         }
-        break;        
+        break;
+      case '1.12.0':
+        // perform upgrade
+        if (file_exists(DIR_WS_INCLUDES . 'installers/fec/1_12_1.php')) {
+          include_once(DIR_WS_INCLUDES . 'installers/fec/1_12_1.php');
+          $fec_version = '1.12.1';          
+        }
+        break;                
       default:
         $fec_version = $current_version;
         // break all the loops
