3481e63675f921f8436cff4b0a8cea62ca275353
cyrobmm <cyrobmm@gmail.com>
2013-06-14 16:00:43 -0700

    Merge branch 'master' of https://github.com/numinix/fec
    Conflicts:
    
    catalog/includes/templates/YOUR_TEMPLATE/templates/tpl_checkout_stacked.
    php

---
+++ catalog/YOUR_ADMIN/includes/init_includes/init_fec_config.php
@@ -0,0 +1,29 @@
+<?php
+  if (!defined('IS_ADMIN_FLAG')) {
+    die('Illegal Access');
+  }
+  // add upgrade script
+  $fec_version = (defined('FAST_AND_EASY_CHECKOUT_VERSION') ? FAST_AND_EASY_CHECKOUT_VERSION : 'new');
+  $current_version = '1.12.0';
+  while ($fec_version != $current_version) {
+    switch($fec_version) {
+      case 'new':
+        // perform upgrade
+        if (file_exists(DIR_WS_INCLUDES . 'installers/fec/new.php')) {
+          include_once(DIR_WS_INCLUDES . 'installers/fec/new.php');
+          $fec_version = '1.12.0';          
+        }
+        break;
+      case '1.11.2':
+        // perform upgrade
+        if (file_exists(DIR_WS_INCLUDES . 'installers/fec/1_12_0.php')) {
+          include_once(DIR_WS_INCLUDES . 'installers/fec/1_12_0.php');
+          $fec_version = '1.12.0';          
+        }
+        break;        
+      default:
+        $fec_version = $current_version;
+        // break all the loops
+        break 2;      
+    }
+  }
--- catalog/includes/templates/YOUR_TEMPLATE/templates/tpl_checkout_stacked.php
+++ catalog/includes/templates/YOUR_TEMPLATE/templates/tpl_checkout_stacked.php
@@ -193,9 +193,18 @@
          include_once(zen_get_file_directory(DIR_WS_LANGUAGES . $_SESSION['language'] .
                 '/modules/order_total/', $value, 'false'));
          include_once(DIR_WS_MODULES . "order_total/" . $value);
+<<<<<<< HEAD
          $wrap_mod = new ot_giftwrap_checkout(); 
+         $products_wrap_fee = $wrap_mod->get_products_wrap_fee();
          $use_gift_wrap = true;
          if ($wrap_mod->check()) {
+=======
+         $wrap_mod = new ot_giftwrap_checkout();
+         if (is_object($wrap_mod)) { 
+	         $products_wrap_fee = $wrap_mod->get_products_wrap_fee();
+	         $use_gift_wrap = true;
+	         if ($wrap_mod->check()) {
+>>>>>>> b59e306a475200255970c407cb005ccdfe68051e
       ?>
             <fieldset class="fec-table-items fec-table-gifts" id="gift_wrap">
                 <legend><?php echo GIFT_WRAP_HEADING; ?></legend>
@@ -212,15 +221,16 @@
                 </tr> -->
       <?php  
              // now loop thru all products to display quantity and price
-         $prod_count = 1; 
+      			 $prod_count = 1; 
       // tsg_logger($order->products); 
-         for ($i=0, $n=sizeof($order->products); $i<$n; $i++) {
+		         for ($i=0, $n=sizeof($order->products); $i<$n; $i++) {
 
-            echo '<div class="fec-box-check-radio">';
+<<<<<<< HEAD
+            
 
            for ($q = 1; $q <= $order->products[$i]['qty']; $q++) {
               
-              // echo '<td>' ;
+              echo '<div class="fec-box-check-radio">';
               
               // gift wrap setting
                // echo '<td>'; 
@@ -239,7 +249,7 @@
                   echo zen_draw_checkbox_field($gift_id,'',$giftChecked, 'id="'.$gift_id .'" onclick="updateForm();"');
                }
               
-              echo "<label><span class='fec-gift-price'><strong>" . $order->products[$i]['price'] . "</strong></span>" . $order->products[$i]['name'];
+              echo "<label><span class='fec-gift-price'><strong>" . $currencies->format($products_wrap_fee[zen_get_prid($order->products[$i]['id'])]) . "</strong></span>" . $order->products[$i]['name'];
 
               // if there are attributes, loop thru them and display one per line
               if (isset($order->products[$i]['attributes']) && sizeof($order->products[$i]['attributes']) > 0 ) {
@@ -251,9 +261,46 @@
               } // endif attribute-info
               
               echo "</label>";
+                echo "</div>";
+=======
+		           echo '<div class="fec-box-check-radio">';
+
+		           for ($q = 1; $q <= $order->products[$i]['qty']; $q++) {
+		              
+									// echo '<td>' ;
+		            
+		            // gift wrap setting
+		             // echo '<td>'; 
+		             $prid = $order->products[$i]['id'];
+		             if (zen_get_products_virtual($order->products[$i]['id'])) {
+		                echo GIFT_WRAP_NA;
+		             } else if (DOWNLOAD_ENABLED && product_attributes_downloads_status($order->products[$i]['id'], $order->products[$i]['attributes'])) {
+		                echo GIFT_WRAP_NA; 
+		             } else if ($wrap_mod->exclude_product($prid)) {
+		                echo GIFT_WRAP_NA; 
+		             } else if ($wrap_mod->exclude_category($prid)) {
+		                echo GIFT_WRAP_NA; 
+		             } else { 
+		                $gift_id = "wrap_prod_" . $prod_count;
+		                if (isset($_SESSION[$gift_id]) && $_SESSION[$gift_id] != '') $giftChecked = true; else $giftChecked = false;
+		                echo zen_draw_checkbox_field($gift_id,'',$giftChecked, 'id="'.$gift_id .'" onclick="updateForm();"');
+		             }
+		            
+		              echo "<label><span class='fec-gift-price'><strong>" . $currencies->format($products_wrap_fee[zen_get_prid($order->products[$i]['id'])]) . "</strong></span>" . $order->products[$i]['name'];
+
+		              // if there are attributes, loop thru them and display one per line
+		              if (isset($order->products[$i]['attributes']) && sizeof($order->products[$i]['attributes']) > 0 ) {
+		                  echo '<ul class="cartAttribsList">';
+		                  for ($j=0, $n2=sizeof($order->products[$i]['attributes']); $j<$n2; $j++) {
+		                      echo '<li>' . $order->products[$i]['attributes'][$j]['option'] . ': ' . nl2br($order->products[$i]['attributes'][$j]['value']) . '</li>'; 
+		                  } // end loop
+		                  echo '</ul>';
+		              } // endif attribute-info
+		              
+		              echo "</label>";
                 
+>>>>>>> b59e306a475200255970c407cb005ccdfe68051e
       ?>
-            </div>
       <?php
                   $prod_count++; 
                }
@@ -261,7 +308,8 @@
       ?>
       </fieldset>
       <?php
-         }  
+      		}
+			  }  
       ?>
       <?php }} ?>
       <!-- eof Gift Wrap -->
