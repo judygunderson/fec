785e08fc0dd9fa49afeef4f95e302116317a6350
cyrobmm <cyrobmm@gmail.com>
2013-06-24 09:59:58 -0700

    Fixing Privacy Policy and Checkout Gap between boxes

--- catalog/includes/templates/YOUR_TEMPLATE/templates/tpl_login_default.php
+++ catalog/includes/templates/YOUR_TEMPLATE/templates/tpl_login_default.php
@@ -71,7 +71,7 @@
       	    	
       	    	<?php echo zen_draw_form('create_account', zen_href_link(FILENAME_LOGIN, '', 'SSL'), 'post', 'onsubmit="return check_form(create_account);"') . '<div>' . zen_draw_hidden_field('action', 'process') . zen_draw_hidden_field('email_format', $email_format); ?>
                     <?php require($template->get_template_dir('tpl_modules_create_account.php',DIR_WS_TEMPLATE, $current_page_base,'templates'). '/tpl_modules_create_account.php'); ?>
-      		    		      <div class="buttonRow forward"><?php echo zen_image_submit(BUTTON_IMAGE_CREATE_ACCOUNT, BUTTON_CREATE_ACCOUNT_ALT); ?></div>
+      		    		      <div class="buttonRow forward"><?php echo zen_image_submit(BUTTON_CONTINUE_ALT, BUTTON_CONTINUE_ALT); ?></div>
       		    	   </div>
       	    	</form>
     	    </fieldset>
@@ -137,7 +137,7 @@
                 <?php 
                     if ($ec_button_enabled) {
                 ?>
-                        <fieldset class="fec-fieldset">
+                        <fieldset class="fec-fieldset fec-paypal">
                             <legend><?php echo HEADING_PAYPAL; ?></legend>
                             <span class="fec-fieldset-legend"><?php echo HEADING_PAYPAL; ?></span>
                             
@@ -171,42 +171,45 @@
       </div>
     <?php } elseif (USE_SPLIT_LOGIN_MODE == 'True' || $ec_button_enabled) { ?>
       <!--BOF PPEC split login- DO NOT REMOVE-->
-      <fieldset class="floatingBox back">
+      <fieldset class="fec-fieldset fec-box">
           <legend><?php echo HEADING_NEW_CUSTOMER_SPLIT; ?></legend>
+          <span class="fec-fieldset-legend"><?php echo HEADING_NEW_CUSTOMER_SPLIT; ?></span>
           <?php // ** BEGIN PAYPAL EXPRESS CHECKOUT ** ?>
           <?php if ($ec_button_enabled) { ?>
-          <div class="information"><?php echo TEXT_NEW_CUSTOMER_INTRODUCTION_SPLIT; ?></div>
+          <div class="fec-information"><?php echo TEXT_NEW_CUSTOMER_INTRODUCTION_SPLIT; ?></div>
 
           <div class="center"><?php require(DIR_FS_CATALOG . DIR_WS_MODULES . 'payment/paypal/tpl_ec_button.php'); ?></div>
           <hr />
       
-      <?php echo TEXT_NEW_CUSTOMER_POST_INTRODUCTION_DIVIDER; ?>
-      <?php } ?>
-      <?php // ** END PAYPAL EXPRESS CHECKOUT ** ?>
-      <div class="information"><?php echo TEXT_NEW_CUSTOMER_POST_INTRODUCTION_SPLIT; ?></div>
+          <?php echo TEXT_NEW_CUSTOMER_POST_INTRODUCTION_DIVIDER; ?>
+          <?php } ?>
+          <?php // ** END PAYPAL EXPRESS CHECKOUT ** ?>
+          <div class="fec-information"><?php echo TEXT_NEW_CUSTOMER_POST_INTRODUCTION_SPLIT; ?></div>
 
-      <?php echo zen_draw_form('create', zen_href_link(FILENAME_CREATE_ACCOUNT, '', 'SSL')); ?>
-      <div class="buttonRow forward"><?php echo zen_image_submit(BUTTON_IMAGE_CREATE_ACCOUNT, BUTTON_CREATE_ACCOUNT_ALT); ?></div>
-      </form>
+          <?php echo zen_draw_form('create', zen_href_link(FILENAME_CREATE_ACCOUNT, '', 'SSL')); ?>
+            <div class="buttonRow forward"><?php echo zen_image_submit(BUTTON_IMAGE_CREATE_ACCOUNT, BUTTON_CREATE_ACCOUNT_ALT); ?></div>
+          </form>
       </fieldset>
 
-      <fieldset class="floatingBox forward">
-      <legend><?php echo HEADING_RETURNING_CUSTOMER_SPLIT; ?></legend>
-      <div class="information"><?php echo TEXT_RETURNING_CUSTOMER_SPLIT; ?></div>
+      <fieldset class="fec-fieldset fec-box">
+          <legend><?php echo HEADING_RETURNING_CUSTOMER_SPLIT; ?></legend>
+          <span class="fec-fieldset-legend"><?php echo HEADING_NEW_CUSTOMER_SPLIT; ?></span>
 
-      <?php echo zen_draw_form('login', zen_href_link(FILENAME_LOGIN, 'action=process', 'SSL')); ?>
-      <label class="inputLabel" for="login-email-address"><?php echo ENTRY_EMAIL_ADDRESS; ?></label>
-      <?php echo zen_draw_input_field('email_address', '', 'size="18" id="login-email-address"'); ?>
-      <br class="clearBoth" />
+          <div class="fec-information"><?php echo TEXT_RETURNING_CUSTOMER_SPLIT; ?></div>
 
-      <label class="inputLabel" for="login-password"><?php echo ENTRY_PASSWORD; ?></label>
-      <?php echo zen_draw_password_field('password', '', 'size="18" id="login-password"'); ?>
-      <?php echo zen_draw_hidden_field('securityToken', $_SESSION['securityToken']); ?>
-      <br class="clearBoth" />
+          <?php echo zen_draw_form('login', zen_href_link(FILENAME_LOGIN, 'action=process', 'SSL')); ?>
+              <label class="inputLabel" for="login-email-address"><?php echo ENTRY_EMAIL_ADDRESS; ?></label>
+              <?php echo zen_draw_input_field('email_address', '', 'size="18" id="login-email-address"'); ?>
+              <br class="clearBoth" />
 
-      <div class="buttonRow forward"><?php echo zen_image_submit(BUTTON_IMAGE_LOGIN, BUTTON_LOGIN_ALT); ?></div>
-      <div class="buttonRow back important"><?php echo '<a href="' . zen_href_link(FILENAME_PASSWORD_FORGOTTEN, '', 'SSL') . '">' . TEXT_PASSWORD_FORGOTTEN . '</a>'; ?></div>
-      </form>
+              <label class="inputLabel" for="login-password"><?php echo ENTRY_PASSWORD; ?></label>
+              <?php echo zen_draw_password_field('password', '', 'size="18" id="login-password"'); ?>
+              <?php echo zen_draw_hidden_field('securityToken', $_SESSION['securityToken']); ?>
+              <br class="clearBoth" />
+
+              <div class="buttonRow forward"><?php echo zen_image_submit(BUTTON_IMAGE_LOGIN, BUTTON_LOGIN_ALT); ?></div>
+              <div class="buttonRow back important"><?php echo '<a href="' . zen_href_link(FILENAME_PASSWORD_FORGOTTEN, '', 'SSL') . '">' . TEXT_PASSWORD_FORGOTTEN . '</a>'; ?></div>
+          </form>
       </fieldset>
       <!--EOF PPEC split login- DO NOT REMOVE-->
     <?php } else { ?>
@@ -219,50 +222,55 @@
         }
       ?>
       <?php echo zen_draw_form('login', zen_href_link(FILENAME_LOGIN, 'action=process', 'SSL')); ?>
-      <fieldset>
-      <legend><?php echo HEADING_RETURNING_CUSTOMER; ?></legend>
-      <label class="inputLabel" for="login-email-address"><?php echo ENTRY_EMAIL_ADDRESS; ?></label>
-      <?php echo zen_draw_input_field('email_address', '', zen_set_field_length(TABLE_CUSTOMERS, 'customers_email_address', '40') . ' id="login-email-address"'); ?>
-      <div class="clearBoth"></div>
-      <?php if (FEC_CONFIRM_EMAIL == 'true') { ?>
-        <label class="inputLabel" for="login-email-address-confirm"><?php echo ENTRY_EMAIL_ADDRESS_CONFIRM; ?></label>
-        <?php echo zen_draw_input_field('email_address_confirm', '', zen_set_field_length(TABLE_CUSTOMERS, 'customers_email_address', '40') . ' id="login-email-address-confirm"'); ?>
-        <div class="clearBoth"></div>    
-      <?php } ?>
-      <label class="inputLabel" for="login-password"><?php echo ENTRY_PASSWORD; ?></label>
-      <?php echo zen_draw_password_field('password', '', zen_set_field_length(TABLE_CUSTOMERS, 'customers_password') . ' id="login-password"'); ?>
-      <div class="clearBoth"></div>
-      <?php 
-        if (PROJECT_VERSION_MAJOR > 1 || (PROJECT_VERSION_MAJOR == 1 && substr(PROJECT_VERSION_MINOR, 0, 3) >= 3.8)) { 
-          echo zen_draw_hidden_field('securityToken', $_SESSION['securityToken']);
-        }
-      ?>
+      <fieldset class="fec-fieldset fec-box">
+          <legend><?php echo HEADING_RETURNING_CUSTOMER; ?></legend>
+          <span class="fec-fieldset-legend"><?php echo HEADING_RETURNING_CUSTOMER; ?></span>
+
+          <label class="inputLabel" for="login-email-address"><?php echo ENTRY_EMAIL_ADDRESS; ?></label>
+          <?php echo zen_draw_input_field('email_address', '', zen_set_field_length(TABLE_CUSTOMERS, 'customers_email_address', '40') . ' id="login-email-address"'); ?>
+          <div class="clearBoth"></div>
+          <?php if (FEC_CONFIRM_EMAIL == 'true') { ?>
+            <label class="inputLabel" for="login-email-address-confirm"><?php echo ENTRY_EMAIL_ADDRESS_CONFIRM; ?></label>
+            <?php echo zen_draw_input_field('email_address_confirm', '', zen_set_field_length(TABLE_CUSTOMERS, 'customers_email_address', '40') . ' id="login-email-address-confirm"'); ?>
+            <div class="clearBoth"></div>    
+          <?php } ?>
+          <label class="inputLabel" for="login-password"><?php echo ENTRY_PASSWORD; ?></label>
+          <?php echo zen_draw_password_field('password', '', zen_set_field_length(TABLE_CUSTOMERS, 'customers_password') . ' id="login-password"'); ?>
+          <div class="clearBoth"></div>
+          <?php 
+            if (PROJECT_VERSION_MAJOR > 1 || (PROJECT_VERSION_MAJOR == 1 && substr(PROJECT_VERSION_MINOR, 0, 3) >= 3.8)) { 
+              echo zen_draw_hidden_field('securityToken', $_SESSION['securityToken']);
+            }
+          ?>
       </fieldset>
-      <div class="buttonRow forward"><?php echo zen_image_submit(BUTTON_IMAGE_LOGIN, BUTTON_LOGIN_ALT); ?></div>
-      <div class="buttonRow back important"><?php echo '<a href="' . zen_href_link(FILENAME_PASSWORD_FORGOTTEN, '', 'SSL') . '">' . TEXT_PASSWORD_FORGOTTEN . '</a>'; ?></div>
+          <div class="buttonRow forward"><?php echo zen_image_submit(BUTTON_IMAGE_LOGIN, BUTTON_LOGIN_ALT); ?></div>
+          <div class="buttonRow back important"><?php echo '<a href="' . zen_href_link(FILENAME_PASSWORD_FORGOTTEN, '', 'SSL') . '">' . TEXT_PASSWORD_FORGOTTEN . '</a>'; ?></div>
       </form>
       <div class="clearBoth"></div>
       <!-- BEGIN CHECKOUT WITHOUT ACCOUNT -->
       <?php
         if (FEC_NOACCOUNT_SWITCH == 'true') {
           if ($_SESSION['cart']->count_contents() > 0) { ?>
-             <fieldset>
-            <legend>Checkout Without Account</legend>
-            <?php echo TEXT_RATHER_COWOA; ?>
-            <div class="buttonRow forward">
-            <?php echo "<a href=\"" . zen_href_link(FILENAME_NO_ACCOUNT, '', 'SSL') . "\">"; ?>
-            <?php echo zen_image_button(BUTTON_IMAGE_CONTINUE, BUTTON_CONTINUE_ALT); ?></a></div>
-            <div class="clearBoth"></div>
+             <fieldset class="fec-fieldset fec-box">
+                  <legend>Checkout Without Account</legend>
+                  <span class="fec-fieldset-legend"><?php echo HEADING_RETURNING_CUSTOMER; ?></span>
+                  <?php echo TEXT_RATHER_COWOA; ?>
+                  <div class="buttonRow forward">
+                  <?php echo "<a href=\"" . zen_href_link(FILENAME_NO_ACCOUNT, '', 'SSL') . "\">"; ?>
+                  <?php echo zen_image_button(BUTTON_IMAGE_CONTINUE, BUTTON_CONTINUE_ALT); ?></a></div>
+                  <div class="clearBoth"></div>
             </fieldset>
       <?php }} ?>
       <!-- END CHECKOUT WITHOUT ACCOUNT -->
       <?php echo zen_draw_form('create_account', zen_href_link(FILENAME_CREATE_ACCOUNT, '', 'SSL'), 'post', 'onsubmit="return check_form(create_account);"') . '<div>' . zen_draw_hidden_field('action', 'process') . zen_draw_hidden_field('email_pref_html', 'email_format') . '</div>'; ?>
-      <fieldset>
-      <legend><?php echo HEADING_NEW_CUSTOMER; ?></legend>
-      <div class="information"><?php echo TEXT_NEW_CUSTOMER_INTRODUCTION; ?></div>
-      <?php require($template->get_template_dir('tpl_modules_create_account.php',DIR_WS_TEMPLATE, $current_page_base,'templates'). '/tpl_modules_create_account.php'); ?>
-      </fieldset>
-      <div class="buttonRow forward"><?php echo zen_image_submit(BUTTON_IMAGE_SUBMIT, BUTTON_SUBMIT_ALT); ?></div>
+          <fieldset class="fec-fieldset fec-box">
+              <legend><?php echo HEADING_NEW_CUSTOMER; ?></legend>
+              <span class="fec-fieldset-legend"><?php echo HEADING_RETURNING_CUSTOMER; ?></span>
+
+              <div class="fec-information"><?php echo TEXT_NEW_CUSTOMER_INTRODUCTION; ?></div>
+              <?php require($template->get_template_dir('tpl_modules_create_account.php',DIR_WS_TEMPLATE, $current_page_base,'templates'). '/tpl_modules_create_account.php'); ?>
+              </fieldset>
+          <div class="buttonRow forward"><?php echo zen_image_submit(BUTTON_IMAGE_SUBMIT, BUTTON_SUBMIT_ALT); ?></div>
       </form>
       <!--EOF normal login-->
     <?php } ?>
