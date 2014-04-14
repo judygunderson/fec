b12e1831f59d017b3782db7b09e5296cd9236ea2
numinix <jlew@numinix.com>
2014-04-14 13:16:02 -0700

    v1.14.6 - bug fix when shipping and billing do not match

--- catalog/YOUR_ADMIN/includes/init_includes/init_fec_config.php
+++ catalog/YOUR_ADMIN/includes/init_includes/init_fec_config.php
@@ -5,7 +5,7 @@ if (!defined('IS_ADMIN_FLAG'))
   }
 // add upgrade script
 $fec_version     = (defined('FAST_AND_EASY_CHECKOUT_VERSION') ? FAST_AND_EASY_CHECKOUT_VERSION : 'new');
-$current_version = '1.14.5'; // change this each time a new version is ready to release
+$current_version = '1.14.6'; // change this each time a new version is ready to release
 while ($fec_version != $current_version)
   {
     switch ($fec_version)
@@ -178,7 +178,19 @@ while ($fec_version != $current_version)
             else
               {
                 break 2;
-              }                             
+              }
+        case '1.14.5':
+            // perform upgrade
+            if (file_exists(DIR_WS_INCLUDES . 'installers/fec/1_14_6.php'))
+              {
+                include_once(DIR_WS_INCLUDES . 'installers/fec/1_14_6.php');
+                $fec_version = '1.14.6';
+                break;
+              }
+            else
+              {
+                break 2;
+              }                                           
         default:
             $fec_version = $current_version;
             // break all the loops
