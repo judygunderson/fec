04266eda191cfb9db2cb6c7389fdd0535111592d
Larry Bislew <bislewl@gmail.com>
2014-11-14 10:33:39 -0600

    upgrade Numinix Functions File

--- catalog/YOUR_ADMIN/includes/functions/extra_functions/numinix_functions.php
+++ catalog/YOUR_ADMIN/includes/functions/extra_functions/numinix_functions.php
@@ -1,20 +1,24 @@
 <?php
   if (!function_exists('nmx_check_field')) {
-    function nmx_check_field($tableName,$columnName){
+    function nmx_check_field($tableName,$columnName,$add = false,$column_type='VARCHAR(72) NULL default NULL'){
         global $db;
+        
+      $return = false;
         //Getting table fields through mysql built in function, passing db name and table name
       $tableFields = $db->metaColumns($tableName);
       
-      $columnName = strtoupper($columnName);
+      $columnNameUpper = strtoupper($columnName);
       //loop to traverse tableFields result set
       foreach($tableFields as $key=>$value) 
       {    
-          if($key == $columnName){
-              return true;
+          if($key == $columnNameUpper){
+              $return = true;
           }
       }
-      //end of loop
-      return false;
-      
+      if($add != false && $return == false){
+          $db->Execute("ALTER TABLE " . $tableName . " ADD ".$columnName." ".$column_type.";");
+          $return = true;
+      }
+      return $return;
     } //end of function
   }
