c6d6b12d3888ede4f913ce57a0cbaf3137cbe19f
Larry Bislew <bislewl@gmail.com>
2014-12-10 12:37:34 -0600

    Fix bug in uninstall to support both FEAC_VERSION and FAST_AND_EASY_CHECKOUT_VERSION

--- uninstall.sql
+++ uninstall.sql
@@ -8,3 +8,11 @@ DELETE FROM configuration_group WHERE configuration_group_id = @configuration_gr
 
 #Zen Cart v1.5.0+ only Below! Skip if using an older version!
 DELETE FROM admin_pages WHERE page_key = 'configFastandEasyCheckout' LIMIT 1;
+
+SET @configuration_group_id=0;
+SELECT @configuration_group_id:=configuration_group_id
+FROM configuration
+WHERE configuration_key= 'FEAC_VERSION'
+LIMIT 1;
+DELETE FROM configuration WHERE configuration_group_id = @configuration_group_id;
+DELETE FROM configuration_group WHERE configuration_group_id = @configuration_group_id;
